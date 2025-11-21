8df26c4c8c29e250a3eee5ae81ea7460bc7c6386
mikhail579-c <mikhail.moh222@gmail.com>
2025-11-21 14:37:15 +0700

    Add files via upload

---
+++ Tipedata.flood.cpp
@@ -0,0 +1,20 @@
+# include  <iostream>
+
+using namespace std;
+
+int main()
+{
+    float angka1;
+    double angka2;
+    long angka3;
+
+    angka1 = 5,55;
+    angka2 = 89;
+    angka3= 281234567;
+
+    cout << "angka (float) pendek: " << angka1 << endl;
+    cout << "angka (double) nilai: " << angka2 << endl;
+    cout << "angka (long) panjang: " << angka3 << endl;
+
+    return 0;
+}
---
+++ bolean1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool var1 = true;
+	bool var2 = false;
+
+	cout << "hasilnya var1 = " << var1 << endl;
+	cout << "hasilnya var2 = " << var2 << endl;
+
+	return 0;
+}
