7c6834d7b5ca845187fbafc062be8c94716da09f
mikhail579-c <mikhail.moh222@gmail.com>
2025-12-07 19:02:28 +0700

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -7,13 +7,15 @@ int main()
     char karakter[7] = {'M', 'I', 'K', 'H', 'A', 'I', 'L'};
 
    cout << "huruf pertama : " << karakter[0] << endl;
-   cout << "huruf kedua : " << karakter[1] << endl;
-   cout << "huruf ketiga : " << karakter[2] << endl;
+   cout << "huruf kedua   : " << karakter[1] << endl;
+   cout << "huruf ketiga  : " << karakter[2] << endl;
    cout << "huruf keempat : " << karakter[3] << endl;
-   cout << "huruf kelima : " << karakter[4] << endl;
-   cout << "huruf kelima : " << karakter[5] << endl;
-   cout << "huruf kelima : " << karakter[6] << endl;
+   cout << "huruf kelima  : " << karakter[4] << endl;
+   cout << "huruf keenam  : " << karakter[5] << endl;
+   cout << "huruf ketujuh : " << karakter[6] << endl;
+   
 
    return 0;
 
 }
+
