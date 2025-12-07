d0c45601878f3015deb5d0a51655a29b20d29a1f
mikhail579-c <mikhail.moh222@gmail.com>
2025-12-07 19:00:46 +0700

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-    char karakter[5] = {'M', 'I', 'R', 'Z', 'A'};
+    char karakter[7] = {'M', 'I', 'K', 'H', 'A', 'I', 'L'};
 
    cout << "huruf pertama : " << karakter[0] << endl;
    cout << "huruf kedua : " << karakter[1] << endl;
    cout << "huruf ketiga : " << karakter[2] << endl;
    cout << "huruf keempat : " << karakter[3] << endl;
    cout << "huruf kelima : " << karakter[4] << endl;
+   cout << "huruf kelima : " << karakter[5] << endl;
+   cout << "huruf kelima : " << karakter[6] << endl;
 
    return 0;
+
 }
